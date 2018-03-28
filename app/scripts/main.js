4c01d2cc8bd034d15501f1daab2895a1655ae505
cdtkl01 <1011377254@qq.com>
2018-03-28 18:13:56 +0800

    更新

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,15 @@
 var play = null,
+    play1 = null,
     time = null,
     index = 0,
-    timer = null,
-    timer1 = null,
+    index1 = 0,
     number = 0;
 $(document).ready(function () {
 
     autoplay();
+    autoplay1();
 
-    autoplaynew();
+    // autoplaynew();
 
 
     // 获取手指在轮播图元素上的一个滑动方向（左右）
@@ -17,7 +18,9 @@ $(document).ready(function () {
     huadong($carousels);
 
     var $carousels1 = $('.storedetail');
-    huadongnew($carousels1);
+    huadong1($carousels1);
+
+
 });
 
 function huadong(tt) {
@@ -28,20 +31,21 @@ function huadong(tt) {
     // 注册滑动事件
     rollthing.on('touchstart', function (e) {
         // 手指触摸开始时记录一下手指所在的坐标x
-        startX=0;
+        startX = 0;
         startX = e.originalEvent.touches[0].clientX;
 
     });
     rollthing.on('touchmove', function (e) {
-        endX=0;
+        e.preventDefault();
+        endX = 0;
         // 目的是：记录手指离开屏幕一瞬间的位置 ，用move事件重复赋值
         endX = e.originalEvent.touches[0].clientX;
     });
     rollthing.on('touchend', function (e) {
         //结束触摸一瞬间记录手指最后所在坐标x的位置 endX
         //比较endX与startX的大小，并获取每次运动的距离，当距离大于一定值时认为是有方向的变化
-        var distance =0;
-        if(endX!=null){
+        var distance = 0;
+        if (endX != null) {
             distance = Math.abs(startX - endX);
         }
         clearInterval(play);
@@ -57,9 +61,9 @@ function huadong(tt) {
                 $(this).carousel(startX > endX ? show(0) : show(1));
             }
 
-            startX=null;
-            endX=null;
-            return distance=null;
+            startX = null;
+            endX = null;
+
 
             var count = 0;
             var outTime = 7;//秒
@@ -72,62 +76,121 @@ function huadong(tt) {
                     autoplay();
                 }
             }
-        }else {
+
+
+        } else {
             clearInterval(play);
         }
     })
 }
 
-function huadongnew(ll) {
-    var rollthing = ll;
+function huadong1(tt) {
+    var rollthing = tt;
     var startX, endX;
     // 在滑动的一定范围内，才切换图片
-    var offset = 50;
+    var offset = 60;
     // 注册滑动事件
-
     rollthing.on('touchstart', function (e) {
         // 手指触摸开始时记录一下手指所在的坐标x
+        startX = 0;
         startX = e.originalEvent.touches[0].clientX;
 
     });
     rollthing.on('touchmove', function (e) {
+        e.preventDefault();
+        endX = 0;
         // 目的是：记录手指离开屏幕一瞬间的位置 ，用move事件重复赋值
-
         endX = e.originalEvent.touches[0].clientX;
     });
     rollthing.on('touchend', function (e) {
-        console.log(endX);
-        // 结束触摸一瞬间记录手指最后所在坐标x的位置 endX
-        // 比较endX与startX的大小，并获取每次运动的距离，当距离大于一定值时认为是有方向的变化
-
-        var distance = Math.abs(startX - endX);
+        //结束触摸一瞬间记录手指最后所在坐标x的位置 endX
+        //比较endX与startX的大小，并获取每次运动的距离，当距离大于一定值时认为是有方向的变化
+        var distance = 0;
+        if (endX != null) {
+            distance = Math.abs(startX - endX);
+        }
+        clearInterval(play1);
         if (distance > offset) {
-            if ($('.storedetail').offset().left - 15 == -($('.shownews').width())) {
+
+            if ($('.shownews').attr('data-id') == 2) {
                 //说明有方向的变化
                 //根据获得的方向 判断是上一张还是下一张出现
-                $(this).carousel(startX > endX ? shownew(2, true) : shownew(0, false));
-            } else if ($('.storedetail').offset().left - 15 == -($('.shownews').width() * 2)) {
-                $(this).carousel(startX > endX ? clearInterval(time) : shownew(1, false));
+                $(this).carousel(startX > endX ? show1(2) : show1(0));
+            } else if ($('.shownews').attr('data-id') == 1) {
+                $(this).carousel(startX > endX ? show1(1) : show1(2));
             } else {
-                $(this).carousel(startX > endX ? shownew(1, true) : clearInterval(time));
+                $(this).carousel(startX > endX ? show1(0) : show1(1));
             }
-
             startX = null;
-            var count = 0;
-            var outTime = 7;//秒
+            endX = null;
+
+            var count1 = 0;
+            var outTime1 = 7;//秒
             window.setInterval(go, 1000);
 
             function go() {
-                count++;
-                if (count == outTime) {
-                    clearInterval(time);
-                    clearInterval(timer);
-                    autoplaynew();
+                count1++;
+                if (count1 == outTime1) {
+                    clearInterval(play1);
+                    autoplay1();
                 }
             }
+
+        } else {
+            clearInterval(play1);
         }
     })
 }
+
+// function huadongnew(ll) {
+//     var rollthing = ll;
+//     var startX, endX;
+//     // 在滑动的一定范围内，才切换图片
+//     var offset = 50;
+//     // 注册滑动事件
+//
+//     rollthing.on('touchstart', function (e) {
+//         // 手指触摸开始时记录一下手指所在的坐标x
+//         startX = e.originalEvent.touches[0].clientX;
+//
+//     });
+//     rollthing.on('touchmove', function (e) {
+//         // 目的是：记录手指离开屏幕一瞬间的位置 ，用move事件重复赋值
+//
+//         endX = e.originalEvent.touches[0].clientX;
+//     });
+//     rollthing.on('touchend', function (e) {
+//         // 结束触摸一瞬间记录手指最后所在坐标x的位置 endX
+//         // 比较endX与startX的大小，并获取每次运动的距离，当距离大于一定值时认为是有方向的变化
+//
+//         var distance = Math.abs(startX - endX);
+//         if (distance > offset) {
+//             if ($('.storedetail').offset().left - 15 == -($('.shownews').width())) {
+//                 //说明有方向的变化
+//                 //根据获得的方向 判断是上一张还是下一张出现
+//                 $(this).carousel(startX > endX ? shownew(2, true) : shownew(0, false));
+//             } else if ($('.storedetail').offset().left - 15 == -($('.shownews').width() * 2)) {
+//                 $(this).carousel(startX > endX ? shownew(2, true) : shownew(1, false));
+//             } else {
+//                 $(this).carousel(startX > endX ? shownew(1, true) : shownew(0, false));
+//             }
+//
+//             startX = null;
+//             var count = 0;
+//             var outTime = 7;//秒
+//             window.setInterval(go, 1000);
+//
+//             function go() {
+//                 count++;
+//                 if (count == outTime) {
+//                     clearInterval(time);
+//                     clearInterval(timer);
+//                     autoplaynew();
+//                 }
+//             }
+//         }
+//     })
+// }
 
 // autoplay banner
 function autoplay() {
@@ -137,12 +200,19 @@ function autoplay() {
     }, 5000)
 }
 
+function autoplay1() {
+    play1 = setInterval(function () {
+        index1 == $('#storesdetails>li').length - 1 ? index1 = 0 : index1++;
+        show1(index1);
+    }, 5000)
+}
+
 // showbanner
 function show(a) {
     index = a;
-    var alpha = 0;
     var $Oimgli = $('#bannerimg>li');
     var $Ocharp = $('#numchar>p');
+
 
     $Oimgli.each(function (b, value) {
         $Oimgli.eq(b).removeClass();
@@ -153,32 +223,36 @@ function show(a) {
         $Ocharp.eq(b).removeClass();
         $Ocharp.eq(index).addClass('default');
     })
-}
-
-// autoplaynew的
-function autoplaynew() {
-    var flag = true;
-    time = setInterval(function () {
-        // 判断是否为最后一个
-        if (number == 2) {
-            flag = false;
-            flag ? number++ : number--;
-        } else {
-            if (number == 1 && flag == false) {
-                flag = false;
-                flag ? number++ : number--;
-            } else {
-                flag = true;
-                flag ? number++ : number--;
-            }
+    $.ajax({
+        method: 'get',
+        url: 'http://idp.dev2.wizarcan.com/client/getBannerHome.shtml?buildingId=33',
+        buildingId: 'true',
+        dataType: 'json',
+        success: function (res) {
+            $('#bannerimg>li').each(function (a, b) {
+                $('.detail').html(res.data[index].title);
+            })
+        },
+        error: function () {
+            console.log('调用banner失败');
         }
-        clearInterval(timer);
-        shownew(number, flag);
-    }, 10000)
+    })
 }
 
-function shownew(a, pp) {
-    number = a;
+function show1(a) {
+    index1 = a;
+    var $storeD = $('#storesdetails>li');
+    var $storeR = $('#roll>p');
+
+    $storeD.each(function (b, value) {
+        $storeD.eq(b).removeClass();
+        $storeD.eq(index1).addClass('shownews');
+    })
+
+    $storeR.each(function (b, value) {
+        $storeR.eq(b).removeClass();
+        $storeR.eq(index1).addClass('default');
+    })
 
     $.ajax({
         method: 'get',
@@ -186,62 +260,111 @@ function shownew(a, pp) {
         buildingId: 'true',
         dataType: 'json',
         success: function (res) {
-            console.log(res.data[1])
             var newstorename = $('.newstorename');
             var newstoreads = $('.newstoreads');
-            var ress=[];
-            $('#storesdetails>li').each(function (g, h) {
-                strs=res.data[number].storeImage.split(',');
+            var ress = [];
+            $('#storesdetails>li').each(function (a, b) {
+                strs = res.data[index1].storeImage.split(',');
                 ress.push(strs[0]);
-                console.log(ress);
-                    $('#storesdetails>li').eq(a).css({
+                if ($('#storesdetails>li').attr('data-id') == a + 1) {
+                    $('#storesdetails>li').eq(index1).css({
                         'background': 'url(http://idp.dev2.wizarcan.com/' + ress[0] + ') no-repeat',
                         'background-size': '100% 100%',
-                    }).attr('onclick', 'javascrtpt:window.location.href="' + res.data[number].url + '"');
-                    newstorename.html(res.data[number].brand.replace('#', '  '));
-                    newstoreads.html(res.data[number].address);
-                    console.log(number)
+                    }).attr('onclick', 'javascrtpt:window.location.href="' + res.data[index1].url + '"');
+                    newstorename.html(res.data[index1].brand.replace('#', '  '));
+                    newstoreads.html(res.data[index1].address);
+                    $('.newstoretitle').html(res.data[index1].newTitle1);
+                }
             })
         },
         error: function () {
-            alert('失败');
+            console.log('调用新店开业失败');
         }
     })
-
-    var k = pp;
-
-    // 新店开业的滚动条
-    var roll = $('#roll>p');
-    roll.each(function (b, value) {
-        roll.eq(b).removeClass();
-        roll.eq(number).addClass('default');
-    });
-
-    var iTarget = number * $('.shownews').width();
-    var iSpeed = ($('.shownews').width()) / 500;
-    iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
-    if(isNaN(iSpeed)){
-
-    }else{
-        if (k == true) {
-            timer = setInterval(function () {
-                if ($('#storesdetails').offset().left <= -(( $('.shownews').width() * number) - 15)) {
-                    clearInterval(timer);
-                } else {
-                    var oo = $('#storesdetails').offset().left - 15 - iSpeed + 'px';
-                    $('#storesdetails').css('left', oo)
-                }
-            }, 1);
-        } else {
-            timer1 = setInterval(function () {
-                if ($('#storesdetails').offset().left == -(( $('.shownews').width() * number) - 15)) {
-                    clearInterval(timer1);
-                } else {
-                    var xx = $('#storesdetails').offset().left - 15 + iSpeed + 'px';
-                    $('#storesdetails').css('left', xx);
-                }
-            }, 1);
-        }
-    }
-    // 判断number是否为0，放置位置
 }
+
+// autoplaynew的
+// function autoplaynew() {
+//     var flag = true;
+//     time = setInterval(function () {
+//         // 判断是否为最后一个
+//         if (number == 2) {
+//             flag = false;
+//             flag ? number++ : number--;
+//         } else {
+//             if (number == 1 && flag == false) {
+//                 flag = false;
+//                 flag ? number++ : number--;
+//             } else {
+//                 flag = true;
+//                 flag ? number++ : number--;
+//             }
+//         }
+//         clearInterval(timer);
+//         shownew(number, flag);
+//     }, 10000)
+// }
+//
+// function shownew(a, pp) {
+//     number = a;
+//
+//     $.ajax({
+//         method: 'get',
+//         url: 'http://idp.dev2.wizarcan.com/client/getNewStore.shtml?buildingId=33',
+//         buildingId: 'true',
+//         dataType: 'json',
+//         success: function (res) {
+//
+//             var newstorename = $('.newstorename');
+//             var newstoreads = $('.newstoreads');
+//             var ress = [];
+//             $('#storesdetails>li').each(function (g, h) {
+//                 strs = res.data[number].storeImage.split(',');
+//                 ress.push(strs[0]);
+//                 $('#storesdetails>li').eq(a).css({
+//                     'background': 'url(http://idp.dev2.wizarcan.com/' + ress[0] + ') no-repeat',
+//                     'background-size': '100% 100%',
+//                 }).attr('onclick', 'javascrtpt:window.location.href="' + res.data[number].url + '"');
+//                 newstorename.html(res.data[number].brand.replace('#', '  '));
+//                 newstoreads.html(res.data[number].address);
+//             })
+//         },
+//         error: function () {
+//             alert('失败');
+//         }
+//     })
+//
+//
+//     var k = pp;
+//
+//     // 新店开业的滚动条
+//     var roll = $('#roll>p');
+//     roll.each(function (b, value) {
+//         roll.eq(b).removeClass();
+//         roll.eq(number).addClass('default');
+//     });
+//
+//     var iTarget = number * $('.shownews').width();
+//     iSpeed = (($('.shownews').width()) / 500);
+//     iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+//     if (k == true) {
+//         timer = setInterval(function () {
+//             if ($('#storesdetails').offset().left == -(($('.shownews').width() * number) - 15)) {
+//                 clearInterval(timer);
+//             } else {
+//                 var oo = $('#storesdetails').offset().left - 15 - iSpeed + 'px';
+//                 $('#storesdetails').css('left', oo);
+//             }
+//         }, 1);
+//     } else {
+//         timer1 = setInterval(function () {
+//             if ($('#storesdetails').offset().left == -(($('.shownews').width() * number) - 15)) {
+//                 clearInterval(timer1);
+//             } else {
+//                 var xx = $('#storesdetails').offset().left - 15 + iSpeed + 'px';
+//                 $('#storesdetails').css('left', xx);
+//             }
+//         }, 1);
+//     }
+//     // 判断number是否为0，放置位置
+// }
--- app/scripts/port.js
+++ app/scripts/port.js
@@ -5,14 +5,12 @@ $(document).ready(function () {
         buildingId: 'true',
         dataType: 'json',
         success: function (res) {
-            console.log(res.data[1])
             var newstorename = $('.newstorename');
             var newstoreads = $('.newstoreads');
             var ress = [];
             $('#storesdetails>li').each(function (a, b) {
                 strs = res.data[0].storeImage.split(',');
                 ress.push(strs[0]);
-                console.log(ress);
                 if ($('#storesdetails>li').attr('data-id') == a + 1) {
                     $('#storesdetails>li').eq(a).css({
                         'background': 'url(http://idp.dev2.wizarcan.com/' + ress[0] + ') no-repeat',
@@ -20,11 +18,12 @@ $(document).ready(function () {
                     }).attr('onclick', 'javascrtpt:window.location.href="' + res.data[0].url + '"');
                     newstorename.html(res.data[0].brand.replace('#', '  '));
                     newstoreads.html(res.data[0].address);
+                    $('.newstoretitle').html(res.data[0].newTitle1);
                 }
             })
         },
         error: function () {
-            alert('失败');
+            console.log('调用新店开业失败');
         }
     })
     $.ajax({
@@ -43,7 +42,7 @@ $(document).ready(function () {
             })
         },
         error: function (res) {
-            alert(res.msg);
+            console.log('调用热门店铺失败');
         }
     })
     $.ajax({
@@ -58,10 +57,11 @@ $(document).ready(function () {
                     'background': 'url(http://idp.dev2.wizarcan.com/' + $bannerimg + ') no-repeat',
                     'background-size': '100% 100%',
                 }).attr('onclick', 'javascrtpt:window.location.href="' + res.data[a].url + '"');
+                $('.detail').eq(a).html(res.data[0].title);
             })
         },
         error: function () {
-            alert('失败');
+            console.log('调用banner失败');
         }
     })
     $.ajax({
@@ -71,39 +71,42 @@ $(document).ready(function () {
         dataType: 'json',
         success: function (res) {
             var index2 = 0;
+            var guu = [];
             $.each(res.data, function (a, b) {
-                if (res.data[a].detail == '西餐') {
-                    console.log(res.data[a])
-                    for (var i = 0; i < index2 + 1; i++) {
-                        $('.datailsul').append('<li class="container">' +
-                        '                    <div class="detailleft">' +
-                        '                        <h1>' + res.data[a].brand.replace('#', '  ') + '</h1>' +
-                        '                        <h2>' + (res.data[a].discountInfo == '' ? '暂无折扣优惠' : res.data[a].discountInfo) + '</h2>' +
-                        '                        <div class="detailleftbottom">' +
-                        '                            <p>' + res.data[a].detail + (res.data[a].serviceLabels[3]== '0'? '':'预付卡店铺')+'</p>' +
-                        '                            <div class="yellowloacl">' +
-                        '                                <img src="images/icon/loaclyellow.png"/>' +
-                        '                                <h3>' + (((res.data[a].mapArea[0]) == 'M') ? 'M' : 'L' +  res.data[a].mapArea[1] )+ '层</h3>' +
-                            '                            </div>' +
-                            '                            <div class="good">' +
-                            '                                <img src="images/icon/good.png"/>' +
-                            '                                <h3>123</h3>' +
-                            '                            </div>' +
-                            '                        </div>' +
-                            '                    </div>' +
-                            '                    <div class="detailright">' +
-                            '                        <img src="' + (typeof(res.data[a].icon) == 'undefined' ? 'images/icon/Rectangle.png' : 'http://idp.dev2.wizarcan.com/' + res.data[a].icon) + '"/>' +
-                            '                    </div>' +
-                            ((res.data[a].serviceLabels[2])== '0' ? '':'<div class="vipsign" >\n' +
-                                '                        <img src="images/icon/vipicon.png"/>\n' +
-                                '                    </div>')+
-                            '                </li>')
+                guu.push(res.data[a].typeIds);
+                if (typeof(guu[a]) != 'undefined') {
+                    if (guu[a].indexOf('225') != -1) {
+                        for (var i = 0; i < index2 + 1; i++) {
+                            $('.datailsul').append('<li class="container" onclick="javascrtpt:window.location.href=\'onestoredetail.html\'">' +
+                                '                    <div class="detailleft">' +
+                                '                        <h1>' + res.data[a].brand.replace('#', '  ') + '</h1>' +
+                                '                        <h2>' + (res.data[a].discountInfo == '' ? '暂无折扣优惠' : res.data[a].discountInfo) + '</h2>' +
+                                '                        <div class="detailleftbottom">' +
+                                '                            <p>' + res.data[a].detail.substr(0, 5).replace('及', '') + '  ' + ((res.data[a].serviceLabels[3]) == '0' ? '' : '预付卡店铺') + '</p>' +
+                                '                            <div class="yellowloacl">' +
+                                '                                <img src="images/icon/loaclyellow.png"/>' +
+                                '                                <h3>' + (((res.data[a].mapArea[0]) == 'M') ? 'M' : 'L' + res.data[a].mapArea[1]) + '层</h3>' +
+                                '                            </div>' +
+                                '                            <div class="good">' +
+                                '                                <img src="images/icon/good.png"/>' +
+                                '                                <h3>123</h3>' +
+                                '                            </div>' +
+                                '                        </div>' +
+                                '                    </div>' +
+                                '                    <div class="detailright">' +
+                                '                        <img src="' + (typeof(res.data[a].icon) == 'undefined' ? 'images/icon/Rectangle.png' : 'http://idp.dev2.wizarcan.com/' + res.data[a].icon) + '"/>' +
+                                '                    </div>' +
+                                ((res.data[a].serviceLabels[2]) == '0' ? '' : '<div class="vipsign" >\n' +
+                                    '                        <img src="images/icon/vipicon.png"/>\n' +
+                                    '                    </div>') +
+                                '                </li>')
+                        }
                     }
                 }
             })
         },
         error: function () {
-            alert('失败');
+            console.log('调用详情店铺失败');
         }
     })
 })
